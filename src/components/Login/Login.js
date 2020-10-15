6406599e80961de55650f9b150f8049c675fc0b7
MrRiaz <monsurrahmanriaz@gmail.com>
2020-10-15 23:53:35 +0600

    done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,14 +21,23 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email};
             setLoggedInUser(signedInUser);
-            history.replace(from);
+            storeAuthToken();
           }).catch( error => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
+            const errorMessage = error.message;
+            console.log(errorMessage);
           });
     }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+        .then(function(idToken) {
+            sessionStorage.setItem('token', idToken);
+            history.replace(from);
+          }).catch(function(error) {
+            // Handle error
+          });
+    }
+
     return (
         <div>
             <h1>This is Login</h1>
